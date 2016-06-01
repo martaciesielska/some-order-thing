f24535efe3b4621ce4ca03e18167fa933396b2b4
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-06-01 15:48:23 +0300

    Added a test

--- src/tests/SomeOrderThing.Tests/Tests.cs
+++ src/tests/SomeOrderThing.Tests/Tests.cs
@@ -1,6 +1,16 @@
 ﻿namespace SomeOrderThing.Tests
 {
+    using Newtonsoft.Json.Linq;
+    using Xunit;
+
     public class Tests
     {
+        [Fact]
+        public void SerializeAndDeserialize()
+        {
+            var json = Properties.Resources.TestData;
+            var jObject = JObject.Parse(json);
+
+        }
     }
 }
