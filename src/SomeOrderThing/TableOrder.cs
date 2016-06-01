cf075b0fe69cd38c0ca40e1fc83609b6bdcb12b2
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-01 14:50:07 +0200

    Added TableOrder

---
+++ src/SomeOrderThing/TableOrder.cs
@@ -0,0 +1,23 @@
+﻿namespace SomeOrderThing
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class TableOrder
+    {
+        private readonly JObject joey;
+
+        public TableOrder(JObject joey)
+        {
+            this.joey = joey;
+        }
+
+        public int TableNumber
+        {
+            get
+            {
+                return 1;
+            }
+        }
+    }
+}
