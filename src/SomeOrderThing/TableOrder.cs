d093308a9a4418b88c403240b2ce6b098b62d9d0
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-06-01 16:07:37 +0300

    Finished templating the table order

--- src/SomeOrderThing/TableOrder.cs
+++ src/SomeOrderThing/TableOrder.cs
@@ -1,12 +1,12 @@
 ﻿namespace SomeOrderThing
 {
     using System.Collections.Generic;
-    using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
 
     public class TableOrder
     {
         private readonly List<LineItem> lineItems = new List<LineItem>();
+        private readonly List<string> ingredients = new List<string>();
 
         private readonly JObject joey;
 
@@ -26,6 +26,17 @@
         public List<LineItem> LineItems
         {
             get { return this.lineItems; }
+        }
+
+        public decimal Tax { get; set; }
+
+        public decimal Total { get; set; }
+
+        public bool Paid { get; set; }
+
+        public List<string> Ingredients
+        {
+            get { return this.ingredients; }
         }
 
         public class LineItem
