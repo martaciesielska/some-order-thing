ffe7c429a820039257ee5cffc2dabba330750458
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-06-01 16:53:57 +0300

    Working order system

--- src/SomeOrderThing/Program.cs
+++ src/SomeOrderThing/Program.cs
@@ -11,6 +11,8 @@
                 TableNumber = 17,
             };
 
+            order.LineItems.Add(new TableOrder.LineItem { Quantity = 1, Item = "KFC please", Price = 4m });
+
             var cook = new Cook(new DummyHandler());
             var waiter = new Waiter(cook);
             waiter.Handle(order);
