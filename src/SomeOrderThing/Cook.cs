ab94d080e1aba38795c353806d8f0f2d2bdd0026
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-01 15:47:39 +0200

    Added the cook

--- src/SomeOrderThing/Cook.cs
+++ src/SomeOrderThing/Cook.cs
@@ -1,16 +1,22 @@
 ﻿namespace SomeOrderThing
 {
-    public class Waiter
+    using System.Threading;
+
+    public class Cook : IHandleOrder
     {
         private readonly IHandleOrder orderHandler;
 
-        public Waiter(IHandleOrder orderHandler)
+        public Cook(IHandleOrder orderHandler)
         {
             this.orderHandler = orderHandler;
         }
 
         public void Handle(TableOrder order)
         {
+            Thread.Sleep(2000);
+
+            order.Ingredients = "KFC chicken";
+
             this.orderHandler.Handle(order);
         }
     }
