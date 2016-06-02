3212a9527ffa4d05095f2fbcbda13c497c2715d9
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-02 12:35:23 +0200

    Added IPublisher and a skeleton TopicBasedPubSub

---
+++ src/SomeOrderThing/IPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeOrderThing
+{
+    public interface IPublisher
+    {
+        void Publish(string topic, TableOrder order);
+    }
+}
---
+++ src/SomeOrderThing/TopicBasedPubSub.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SomeOrderThing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TopicBasedPubSub : IPublisher
+    {
+        private readonly Dictionary<string, IHandleOrder> handlers;
+
+        public void Publish(string topic, TableOrder order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
