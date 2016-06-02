23cf6905f12063c23824195b62f3746157b7df03
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-02 09:15:48 +0200

    Wrapped cashier and assMan in TaskThreadedHandler

--- src/SomeOrderThing/Program.cs
+++ src/SomeOrderThing/Program.cs
@@ -7,21 +7,23 @@
         public static void Main(string[] args)
         {
             var printer = new PrintingHandler();
-            var cashier = new Cashier(printer);
-            var assMan = new AssistantManager(cashier);
+            var cashier = new TaskThreadedHandler(new Cashier(printer));
+            var assMan = new TaskThreadedHandler(new AssistantManager(cashier));
             var cooks = new[] { new TaskThreadedHandler(new Cook(assMan)), new TaskThreadedHandler(new Cook(assMan)), new TaskThreadedHandler(new Cook(assMan)) };
             var multiplexer = new RoundRobinDispatcher(cooks);
             var waiter = new Waiter(multiplexer);
+
+            foreach (var cook in cooks)
+            {
+                cook.Start();
+            }
+            cashier.Start();
+            assMan.Start();
 
             for (var i = 0; i < 10; i++)
             {
                 var order = new TableOrder(Guid.NewGuid());
                 waiter.Handle(order);
-            }
-
-            foreach (var cook in cooks)
-            {
-                cook.Start();
             }
 
             Console.ReadLine();
@@ -30,6 +32,8 @@
             {
                 cook.Dispose();
             }
+            cashier.Dispose();
+            assMan.Dispose();
         }
     }
 }
