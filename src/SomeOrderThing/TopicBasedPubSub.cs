43966bf3eadc0e96ae3b7f2d668283f723465ed3
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-02 12:39:11 +0200

    Added some code in TopicBasedSubPub

--- src/SomeOrderThing/TopicBasedPubSub.cs
+++ src/SomeOrderThing/TopicBasedPubSub.cs
@@ -6,11 +6,20 @@ namespace SomeOrderThing
 
     public class TopicBasedPubSub : IPublisher
     {
-        private readonly Dictionary<string, IHandleOrder> handlers;
+        private readonly Dictionary<string, IHandleOrder> handlers 
+            = new Dictionary<string, IHandleOrder>();
 
         public void Publish(string topic, TableOrder order)
         {
-            throw new NotImplementedException();
+            if (this.handlers.ContainsKey(topic))
+            {
+                this.handlers[topic].Handle(order);
+            }
+        }
+
+        public void Subscribe(string topic, IHandleOrder handler)
+        {
+            this.handlers.Add(topic, handler);
         }
     }
 }
