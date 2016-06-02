0a7d1211a86d6b3d8d7d4e4aca761b18418dc917
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-06-02 09:11:49 +0300

    Added multiplexer

---
+++ src/SomeOrderThing/Multiplexer.cs
@@ -0,0 +1,21 @@
+﻿namespace SomeOrderThing
+{
+    using System.Collections.Generic;
+
+    public class Multiplexer : IHandleOrder
+    {
+        private readonly IEnumerable<IHandleOrder> orderHandlers;
+
+        public Multiplexer(IEnumerable<IHandleOrder> orderHandlers)
+        {
+            this.orderHandlers = orderHandlers;
+        }
+        public void Handle(TableOrder order)
+        {
+            foreach (var orderHandler in this.orderHandlers)
+            {
+                orderHandler.Handle(order);
+            }
+        }
+    }
+}
