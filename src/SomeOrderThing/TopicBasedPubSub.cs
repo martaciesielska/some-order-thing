712e9d2694611ecf5ee3f407aafd94f75eaae572
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2016-06-02 14:36:02 +0200

    Use dynamic

--- src/SomeOrderThing/TopicBasedPubSub.cs
+++ src/SomeOrderThing/TopicBasedPubSub.cs
@@ -5,21 +5,21 @@
 
     public class TopicBasedPubSub : IPublisher
     {
-        private readonly Dictionary<string, Action<object>> handlers 
-            = new Dictionary<string, Action<object>>();
+        private readonly Dictionary<string, dynamic> handlers 
+            = new Dictionary<string, dynamic>();
 
         public void Publish<T>(T message)
         {
             var topic = message.GetType().Name;
             if (this.handlers.ContainsKey(topic))
             {
-                this.handlers[topic](message);
+                this.handlers[topic].Handle(message);
             }
         }
 
         public void Subscribe<T>(IHandle<T> handler)
         {
-            this.handlers.Add(typeof(T).Name, message => handler.Handle((T)message));
+            this.handlers.Add(typeof(T).Name, handler);
         }
     }
 }
